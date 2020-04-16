68a57841cc163919f7651c5917834569ea798ac8
Michael Kriese <michael.kriese@visualon.de>
2020-04-16 11:51:30 +0200

    chore: reorder messages

--- build/gradle.sh
+++ build/gradle.sh
@@ -2,13 +2,15 @@
 
 set -e
 
-if [ -z ${GRADLE_VERSION+x} ]; then echo "No GRADLE_VERSION defined - skipping" && exit; else echo "Installing gradle $GRADLE_VERSION"; fi
+if [ -z ${GRADLE_VERSION+x} ]; then echo "No GRADLE_VERSION defined - skipping" && exit; fi
 
 if [ -z ${JAVA_VERSION+x} ]; then
   export JAVA_VERSION=8
   DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
   . $DIR/java.sh
 fi
+
+echo "Installing gradle $GRADLE_VERSION"
 
 curl -sL -o gradle.zip https://services.gradle.org/distributions/gradle-$GRADLE_VERSION-bin.zip
 unzip -d /usr/local gradle.zip
