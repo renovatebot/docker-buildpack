c6d6423b3625f68b1a57e09d9e3b414b03e091a3
Michael Kriese <michael.kriese@visualon.de>
2020-04-21 11:40:00 +0200

    fix: wrong docker curl

--- build/docker.sh
+++ build/docker.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-curl -fsSLO https://download.docker.com/linux/static/stable/x86_64/docker-${DOCKER_VERSION}.tgz -o docker.tgz
+curl -sSL https://download.docker.com/linux/static/stable/x86_64/docker-${DOCKER_VERSION}.tgz -o docker.tgz
 tar xzvf docker.tgz --strip 1 -C /usr/local/bin docker/docker
 rm docker.tgz
 
