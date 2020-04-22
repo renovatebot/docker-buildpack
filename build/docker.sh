092e121eba5638985573134395f338a0a56408a0
Michael Kriese <michael.kriese@visualon.de>
2020-04-22 17:30:13 +0200

    fix: add docker group

--- build/docker.sh
+++ build/docker.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+groupadd -g 999 docker
+usermod -aG docker ubuntu
+
 curl -sSL https://download.docker.com/linux/static/stable/x86_64/docker-${DOCKER_VERSION}.tgz -o docker.tgz
 tar xzvf docker.tgz --strip 1 -C /usr/local/bin docker/docker
 rm docker.tgz
