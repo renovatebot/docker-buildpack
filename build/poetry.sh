e5022aee81c9614b3ed82d8de57fdca19f96b5e4
Michael Kriese <michael.kriese@visualon.de>
2020-04-22 16:33:25 +0200

    feat: install python with pyenv

--- build/poetry.sh
+++ build/poetry.sh
@@ -2,18 +2,15 @@
 
 set -e
 
-check_command python3
+check_command python
 
 export POETRY_HOME=/usr/local/poetry
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 - --version ${POETRY_VERSION}
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python - --version ${POETRY_VERSION}
 
 export_path "/usr/local/poetry/bin"
 
 # fix executable flag
 chmod +x /usr/local/poetry/bin/poetry
-
-# fix python version
-sed -i 's/^#!\/usr\/bin\/env python$/#!\/usr\/bin\/env python3/' /usr/local/poetry/bin/poetry
 
 poetry config virtualenvs.in-project false
 poetry --version
