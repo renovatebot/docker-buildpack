4eccd998b3fe3c34c4ca742fb86bed39f7e23a3c
Michael Kriese <michael.kriese@visualon.de>
2020-04-23 08:23:29 +0200

    chore: add go test

--- build/golang.sh
+++ build/golang.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-apt_install bzr mercurial
-
 curl -sSL https://dl.google.com/go/go${GOLANG_VERSION}.linux-amd64.tar.gz --output go.tgz
 tar -C /usr/local -xzf go.tgz
 rm go.tgz
@@ -15,7 +13,6 @@ export_env GOSUMDB off
 export_path "/usr/local/go/bin:$GOPATH/bin"
 
 mkdir -p "$GOPATH/src" "$GOPATH/bin"
-chmod -R 777 "$GOPATH"
 
 go version
 go env
---
+++ test/golang/test/c/go.mod
@@ -0,0 +1,5 @@
+module github.com/renovate-tests/gomod1
+
+go 1.14
+
+require github.com/pkg/errors v0.9.0
