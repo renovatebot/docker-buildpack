976e290ffaf32b66b8b73fbb3e178f69d56d51e2
Michael Kriese <michael.kriese@visualon.de>
2020-05-19 06:53:08 +0200

    feat: use prebuild ruby (#32)

--- src/ruby/build/ruby.sh
+++ src/ruby/build/ruby.sh
@@ -5,28 +5,54 @@ set -e
 check_semver ${RUBY_VERSION}
 
 
-if [[ ! "${MAJOR}" || ! "${MINOR}" ]]; then
+if [[ ! "${MAJOR}" || ! "${MINOR}" || ! "${PATCH}" ]]; then
   echo Invalid version: ${RUBY_VERSION}
   exit 1
 fi
 
-install-apt build-essential libssl-dev libreadline-dev zlib1g-dev
-
-
-if [[ ! -x "$(command -v ruby-build)" ]]; then
-  git clone https://github.com/rbenv/ruby-build.git
-  PREFIX=/usr/local ./ruby-build/install.sh
-  rm -rf ruby-build
+if [[ -d "/usr/local/ruby/${RUBY_VERSION}" ]]; then
+  echo "Skipping, already installed"
+  exit 0
 fi
 
-ruby-build $RUBY_VERSION /usr/local/ruby-${RUBY_VERSION}
+mkdir -p /usr/local/ruby
 
-export_path "\$HOME/.gem/ruby/${MAJOR}.${MINOR}.0/bin:/usr/local/ruby-${RUBY_VERSION}/bin"
+CODENAME=$(. /etc/os-release && echo ${VERSION_ID})
+RUBY_URL="https://github.com/renovatebot/ruby/releases/download"
+
+curl -sSfLo ruby.tar.xz ${RUBY_URL}/${RUBY_VERSION}/ruby-${RUBY_VERSION}-${CODENAME}.tar.xz || echo 'Ignore download error'
+
+if [[ -f ruby.tar.xz ]]; then
+  echo "Using prebuild ruby for ${CODENAME}"
+  install-apt \
+    build-essential \
+    ;
+  tar -C /usr/local/ruby -xf ruby.tar.xz
+  rm ruby.tar.xz
+else
+  echo 'No prebuild ruby found, building from source'
+  install-apt \
+    build-essential \
+    libreadline-dev \
+    libssl-dev \
+    zlib1g-dev \
+    ;
+
+  if [[ ! -x "$(command -v ruby-build)" ]]; then
+    git clone https://github.com/rbenv/ruby-build.git
+    PREFIX=/usr/local ./ruby-build/install.sh
+    rm -rf ruby-build
+  fi
+
+  ruby-build $RUBY_VERSION /usr/local/ruby/${RUBY_VERSION}
+fi
+
+export_path "\$HOME/.gem/ruby/${MAJOR}.${MINOR}.0/bin:/usr/local/ruby/${RUBY_VERSION}/bin"
 
 
 # System settings
-mkdir -p /usr/local/ruby-${RUBY_VERSION}/etc
-cat > /usr/local/ruby-${RUBY_VERSION}/etc/gemrc <<- EOM
+mkdir -p /usr/local/ruby/${RUBY_VERSION}/etc
+cat > /usr/local/ruby/${RUBY_VERSION}/etc/gemrc <<- EOM
 gem: --no-document
 :benchmark: false
 :verbose: true
