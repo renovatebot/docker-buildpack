746fa87fa230220ef49e587a7e1d7e3c75a038af
Michael Kriese <michael.kriese@visualon.de>
2020-06-03 15:07:24 +0200

    feat(rust): use standalone installer

--- src/rust/build/rust.sh
+++ src/rust/build/rust.sh
@@ -2,16 +2,32 @@
 
 set -e
 
+check_semver ${RUST_VERSION}
 
-export_env RUSTUP_HOME /usr/local/rust
-export CARGO_HOME=/usr/local/cargo
 
-curl -sSL https://sh.rustup.rs | sh -s - --no-modify-path --profile minimal --default-toolchain ${RUST_VERSION} -y
+if [[ ! "${MAJOR}" || ! "${MINOR}" || ! "${PATCH}" ]]; then
+  echo Invalid version: ${RUST_VERSION}
+  exit 1
+fi
 
+if [[ -d "/usr/local/rust/${RUST_VERSION}" ]]; then
+  echo "Skipping, already installed"
+  exit 0
+fi
+
+mkdir -p /usr/local/rust
+
+curl -sSfLo rust.tar.gz https://static.rust-lang.org/dist/rust-${RUST_VERSION}-x86_64-unknown-linux-gnu.tar.gz
+mkdir rust
+pushd rust
+tar --strip 1 -xf ../rust.tar.gz
+./install.sh --prefix=/usr/local/rust/${RUST_VERSION} --components=cargo,rust-std-x86_64-unknown-linux-gnu,rustc
+popd
+rm rust.tar.gz
+rm -rf rust
 
 export_env RUST_BACKTRACE 1
-export_path "\$CARGO_HOME/bin:/usr/local/cargo/bin"
+export_path "\$CARGO_HOME/bin:/usr/local/rust/${RUST_VERSION}/bin"
 
-rustup --version
 cargo --version
 rustc --version
