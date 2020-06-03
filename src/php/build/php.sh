e82393e33f3c57bfd6f55ad85dba3de5282b2660
Michael Kriese <michael.kriese@visualon.de>
2020-06-03 19:05:38 +0200

    feat(php): refactor build (#45)

--- src/php/build/php.sh
+++ src/php/build/php.sh
@@ -2,7 +2,19 @@
 
 set -e
 
-echo "deb http://ppa.launchpad.net/ondrej/php/ubuntu bionic main" > /etc/apt/sources.list.d/ondrej-php.list
+check_semver ${PHP_VERSION}
+
+if [[ ! "${MAJOR}" || ! "${MINOR}" ]]; then
+  echo Invalid version: ${PHP_VERSION}
+  exit 1
+fi
+
+
+echo "deb http://ppa.launchpad.net/ondrej/php/ubuntu bionic main" | tee /etc/apt/sources.list.d/ondrej-php.list
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 14AA40EC0831756756D7F66C4F4EA0AAE5267A6C
 
-apt_install php${PHP_VERSION}-cli php${PHP_VERSION}-mbstring php${PHP_VERSION}-curl php${PHP_VERSION}-xml php${PHP_VERSION}-json
+VERSION=${MAJOR}.${MINOR}
+apt_install php${VERSION}-cli php${VERSION}-mbstring php${VERSION}-curl php${VERSION}-xml php${VERSION}-json
+
+
+php -v
