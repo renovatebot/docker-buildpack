0b6a4b19cac54115532917b18b3e59e972e6f324
Michael Kriese <michael.kriese@visualon.de>
2020-07-07 08:15:49 +0200

    fix: add gradle settings

--- src/java/build/gradle.sh
+++ src/java/build/gradle.sh
@@ -8,6 +8,8 @@ curl -sL -o gradle.zip https://services.gradle.org/distributions/gradle-${GRADLE
 unzip -d /usr/local gradle.zip
 rm gradle.zip
 
+export_env GRADLE_OPTS "-Dorg.gradle.parallel=true -Dorg.gradle.configureondemand=true -Dorg.gradle.daemon=false -Dorg.gradle.caching=false"
+
 export_path "/usr/local/gradle-${GRADLE_VERSION}/bin"
 
 gradle --version
