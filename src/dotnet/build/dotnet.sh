27d60c7c1175090caf9a2be23a2430710de72406
Florian Greinacher <fgreinacher@users.noreply.github.com>
2020-10-14 21:02:41 +0000

    Don't install twice

--- src/dotnet/build/dotnet.sh
+++ src/dotnet/build/dotnet.sh
@@ -4,12 +4,24 @@ set -e
 
 check_semver $DOTNET_VERSION
 
+if [[ ! "${MAJOR}" || ! "${MINOR}" || ! "${PATCH}" ]]; then
+  echo Invalid version: ${DOTNET_VERSION}
+  exit 1
+fi
+
+DOTNET_INSTALL_DIR=/usr/local/dotnet-${DOTNET_VERSION}
+
+if [[ -d "${DOTNET_INSTALL_DIR}" ]]; then
+  echo "Skipping, already installed"
+  exit 0
+fi
+
 apt_install libc6 libgcc1 libgssapi-krb5-2 libicu60 libssl1.1 libstdc++6 zlib1g
 
 curl -sSL -o /usr/local/dotnet-install.sh https://dot.net/v1/dotnet-install.sh
-bash /usr/local/dotnet-install.sh --version $DOTNET_VERSION --install-dir /usr/local/dotnet-${DOTNET_VERSION}
+bash /usr/local/dotnet-install.sh --version $DOTNET_VERSION --install-dir ${DOTNET_INSTALL_DIR}
 
-export_path "/usr/local/dotnet-${DOTNET_VERSION}"
-export_env DOTNET_ROOT "/usr/local/dotnet-${DOTNET_VERSION}"
+export_path "${DOTNET_INSTALL_DIR}"
+export_env DOTNET_ROOT "${DOTNET_INSTALL_DIR}"
 
 dotnet help
