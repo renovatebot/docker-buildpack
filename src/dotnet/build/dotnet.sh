bba13a108d29d7fda8da965fe9ff976717fff83c
Florian Greinacher <fgreinacher@users.noreply.github.com>
2020-10-14 20:38:33 +0000

    fix directory

--- src/dotnet/build/dotnet.sh
+++ src/dotnet/build/dotnet.sh
@@ -6,10 +6,10 @@ check_semver $DOTNET_VERSION
 
 apt_install libc6 libgcc1 libgssapi-krb5-2 libicu60 libssl1.1 libstdc++6 zlib1g
 
-curl -sSL -o /usr/local/dotnet/dotnet-install.sh https://dot.net/v1/dotnet-install.sh
-bash /usr/local/dotnet/dotnet-install.sh --version $DOTNET_VERSION --install-dir /usr/local/dotnet/bin/${DOTNET_VERSION}
+curl -sSL -o /usr/local/dotnet-install.sh https://dot.net/v1/dotnet-install.sh
+bash /usr/local/dotnet-install.sh --version $DOTNET_VERSION --install-dir /usr/local/dotnet-${DOTNET_VERSION}
 
-export_path "/usr/local/dotnet/bin/${DOTNET_VERSION}"
-export_env DOTNET_ROOT "/usr/local/dotnet/bin/${DOTNET_VERSION}"
+export_path "/usr/local/dotnet-${DOTNET_VERSION}"
+export_env DOTNET_ROOT "/usr/local/dotnet-${DOTNET_VERSION}"
 
 dotnet help
