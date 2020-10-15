677766812d0b239656802f5b2bfe7087127fc772
Florian Greinacher <fgreinacher@users.noreply.github.com>
2020-10-15 19:39:49 +0200

    feat: add dotnet (#54)
    Co-authored-by: Michael Kriese <michael.kriese@visualon.de>

---
+++ src/dotnet/build/dotnet.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+check_semver $DOTNET_VERSION
+
+if [[ ! "${MAJOR}" || ! "${MINOR}" || ! "${PATCH}" ]]; then
+  echo Invalid version: ${DOTNET_VERSION}
+  exit 1
+fi
+
+DOTNET_INSTALL_DIR=/usr/local/dotnet/${DOTNET_VERSION}
+
+if [[ -d "${DOTNET_INSTALL_DIR}" ]]; then
+  echo "Skipping, already installed"
+  exit 0
+fi
+
+
+mkdir -p /usr/local/dotnet
+apt_install libc6 libgcc1 libgssapi-krb5-2 libicu60 libssl1.1 libstdc++6 zlib1g
+
+curl -sfL https://dot.net/v1/dotnet-install.sh | bash -s - --version $DOTNET_VERSION --install-dir ${DOTNET_INSTALL_DIR}
+
+export_path "${DOTNET_INSTALL_DIR}"
+export_env DOTNET_ROOT "${DOTNET_INSTALL_DIR}"
+export_env DOTNET_CLI_TELEMETRY_OPTOUT "1"
+
+dotnet help
