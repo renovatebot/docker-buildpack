25ae18c271f1bbdc6c2ee20aea328068458e6a8b
Michael Kriese <michael.kriese@visualon.de>
2020-10-15 11:48:14 +0200

    Apply suggestions from code review

--- src/dotnet/build/dotnet.sh
+++ src/dotnet/build/dotnet.sh
@@ -9,17 +9,18 @@ if [[ ! "${MAJOR}" || ! "${MINOR}" || ! "${PATCH}" ]]; then
   exit 1
 fi
 
-DOTNET_INSTALL_DIR=/usr/local/dotnet-${DOTNET_VERSION}
+DOTNET_INSTALL_DIR=/usr/local/dotnet/${DOTNET_VERSION}
 
 if [[ -d "${DOTNET_INSTALL_DIR}" ]]; then
   echo "Skipping, already installed"
   exit 0
 fi
 
+
+mkdir -p /usr/local/dotnet
 apt_install libc6 libgcc1 libgssapi-krb5-2 libicu60 libssl1.1 libstdc++6 zlib1g
 
-curl -sSL -o /usr/local/dotnet-install.sh https://dot.net/v1/dotnet-install.sh
-bash /usr/local/dotnet-install.sh --version $DOTNET_VERSION --install-dir ${DOTNET_INSTALL_DIR}
+curl -sSL https://dot.net/v1/dotnet-install.sh | bash - --version $DOTNET_VERSION --install-dir ${DOTNET_INSTALL_DIR}
 
 export_path "${DOTNET_INSTALL_DIR}"
 export_env DOTNET_ROOT "${DOTNET_INSTALL_DIR}"
