03295487154d1588174a05d4ea4eb26d57c8d559
Michael Kriese <michael.kriese@visualon.de>
2021-01-28 20:45:08 +0100

    Update src/base/build/git.sh
    Co-authored-by: Rhys Arkins <rhys@arkins.net>

--- src/base/build/git.sh
+++ src/base/build/git.sh
@@ -7,7 +7,7 @@ VERSION_CODENAME=$(. /etc/os-release && echo ${VERSION_CODENAME})
 echo "deb http://ppa.launchpad.net/git-core/ppa/ubuntu ${VERSION_CODENAME} main" | tee -a /etc/apt/sources.list.d/git.list
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E1DD270288B4E6030699E45FA1715D88E1DF1F24
 
-# TODO: Only latest version available on launpad :-/
+# TODO: Only latest version available on launchpad :-/
 #apt_install git=1:${GIT_VERSION}*
 apt_install git
 
