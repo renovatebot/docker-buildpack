988e9fde71f180ecd101a8fb3795aee599641741
Michael Kriese <michael.kriese@visualon.de>
2021-02-01 08:35:15 +0100

    fix: wrong condition

--- .github/bin/env.sh
+++ .github/bin/env.sh
@@ -4,13 +4,14 @@ set -e
 
 TARGET_TAG=${TARGET_TAG/-latest/}
 TARGET_TAG=${TARGET_TAG/latest-/}
-echo "TARGET_TAG=${TARGET_TAG}" >> $GITHUB_ENV
+echo "TARGET_TAG=${TARGET_TAG}" | tee -a $GITHUB_ENV
 
 line=$(cat renovate.Dockerfile | grep "as ${IMAGE}")
+re="^FROM (.+?) as ${IMAGE}$"
 
-if [[ ! "${line}" =~ "^FROM (.+) as ${IMAGE}$" ]]; then
+if [[ ! "${line}" =~ $re ]]; then
   echo Not a ubuntu version - aborting: ${line}
   exit 1
 fi
 
-echo "BASE_VERSION=${${BASH_REMATCH[1]}}" >> $GITHUB_ENV
+echo "BASE_VERSION=${BASH_REMATCH[1]}" | tee -a $GITHUB_ENV
